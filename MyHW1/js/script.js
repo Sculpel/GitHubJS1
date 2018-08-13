f580fb790bb39c5e5d35fa9b59c53dfab4f0d7b7
Sculpel <sculpel@gmail.com>
2018-08-13 22:55:19 +0300

    homework1

---
+++ MyHW1/js/script.js
@@ -0,0 +1,15 @@
+		
+a = prompt('Сколько у вас с собой денег?')
+b = prompt('Сколько вы купили яблок')
+v = prompt('Батонов хлеба')
+b1 = prompt('цена яблока')
+b2 = b1*b
+v1 = prompt('цена батона')
+v2 = v1*v
+
+bool = a>=(b2+v2)
+var elem = document.getElementById("content");
+elem.innerHTML = bool 
+
+
+
