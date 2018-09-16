b24e804869f16a9a258f067f3583c7fa140da0e5
Sculpel <sculpel@gmail.com>
2018-09-16 15:23:13 +0300

    form

---
+++ 06_forms/js/script.js
@@ -0,0 +1,14 @@
+console.log('You are at'+window.location);
+
+//Создайте форму, которая будет по сабмиту рассчитывать синус введенного в input угла. 
+//Вам понадобится найти этот метод объекта Math
+
+const form = document.forms[0];
+console.log(form);
+let ug = document.querySelector('.total');
+
+form.onsubmit=function(e) {
+	e.preventDefault();
+	let pp = Math.sin(form.elements.ugol1.value);
+	ug.innerHTML = pp
+} 
